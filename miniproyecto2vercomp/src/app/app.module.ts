591e3e6c443f136fa27cc46bc7828787f14f3f4c
Ninobao <eric.rfl7@gmail.com>
2022-05-04 09:23:24 -0500

    Init app

---
+++ miniproyecto2vercomp/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { SearchComponent } from './search/search.component';
+import { UnheroeComponent } from './unheroe/unheroe.component';
+import { FormComponent } from './form/form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent,
+    SearchComponent,
+    UnheroeComponent,
+    FormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
